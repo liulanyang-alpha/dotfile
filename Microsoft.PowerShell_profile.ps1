96ebfd6aeb8df51215464cbf8e1db911b72b4313
liulanyang-alpha <74061876+liulanyang-alpha@users.noreply.github.com>
2023-12-05 16:28:01 +0800

    Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+
+# copy to  C:\Users\Administrator\Documents\WindowsPowerShell
+Set-Alias ll 'C:\Program Files\Git\usr\bin\ls.exe'
+Set-Alias grep 'C:\Program Files\Git\usr\bin\grep.exe'
+Set-Alias which 'C:\Program Files\Git\usr\bin\which.exe'
+
+Set-Alias find 'C:\Program Files\Git\usr\bin\find.exe'
+Set-Alias mkdir 'C:\Program Files\Git\usr\bin\mkdir.exe'
+
+Set-Alias tar 'C:\Program Files\Git\usr\bin\tar.exe'
+Set-Alias vim 'C:\Program Files\Git\usr\bin\vim.exe'
+Set-Alias fd 'C:\Program Files\Git\usr\bin\fd.exe'
+
